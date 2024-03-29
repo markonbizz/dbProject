156a676345a4f4cdfeb53da95967666455cef1dc
markonbizz <markonbizz@gmail.com>
2024-03-29 16:09:04 +0800

    add backbones

---
+++ index.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        
+        <meta charset="UTF-8">
+        <title>Website Title Goes Here</title>
+
+        <link href="index.css" rel="stylesheet" type="text/css">
+
+    </head>
+    <body>
+
+        <div class="home_topbar">
+        
+            <h1>Website Name Goes Here</h1>
+            <!-- <div class="home_login_button"></div> -->
+        
+        </div>
+
+    </body>
+
+</html>
