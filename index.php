e6707160f4751363f5541e923b6ed1c2f755bf37
MarcusHsieh <markonbizz@gmail.com>
2024-04-14 17:14:54 +0800

    Import Bootstrap to the file.

--- index.php
+++ index.php
@@ -1,25 +1,52 @@
-
 <!DOCTYPE html>
 
-<html>
+<!-------------------------------------------------------------------------------------
 
+    Writing Style:
+    
+    1. Used "<div>" tag to organized elements.
+    2. The inside of tags that have </> to wrap around must leave "1st line" & "last line" blank.
+    3. The gap between each tag must leave blank.
+
+-------------------------------------------------------------------------------------->
+
+<html lang="en">
+
+    <!-- [ Settings ] -->
     <head>
+
+        <!-- [ Website Title ] -->
+        <title>Bootstrap Demo</title>
         
-        <meta charset="UTF-8">
-        <title>Website Title Goes Here</title>
+        <!-- [ Website Attributes ] -->
+        <div class = "index-head-attrs">
 
-        <link href="index.css" rel="stylesheet" type="text/css">
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    </head>
-    <body>
-
-        <div class="home_topbar">
-        
-            <h1>Website Name Goes Here</h1>
-            <!-- <div class="home_login_button"></div> -->
+            <!-- [ Bootstrap Compiled CSS ] -->
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
+                  rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" 
+                  crossorigin="anonymous">
         
         </div>
+    
+    </head>
+    
+    <!-- [ Contents ] -->
+    <body>
 
+        <!-- [ Bootstrap Scripts] -->
+        <script 
+        
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" 
+            crossorigin="anonymous">
+        
+        </script>
+        
+        <div class = client-login></div>
+        
     </body>
 
 </html>
