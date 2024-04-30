cce11b722fd13bf023ccfca210596685bb9bfa8d
markonbizz <markonbizz@gmail.com>
2024-05-01 03:34:06 +0800

    backup old db & save new db

--- db/DEArmory_old.sql
+++ db/DEArmory_old.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1deb3
+-- version 5.1.1deb5ubuntu1
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Apr 30, 2024 at 07:33 PM
--- Server version: 10.11.7-MariaDB-2ubuntu2
--- PHP Version: 8.3.6
+-- Generation Time: Apr 30, 2024 at 03:44 AM
+-- Server version: 10.6.16-MariaDB-0ubuntu0.22.04.1
+-- PHP Version: 8.1.2-1ubuntu2.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,21 +28,20 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `User` (
-  `Account` text NOT NULL,
-  `RealName` text NOT NULL,
-  `Email` text NOT NULL,
-  `Birthday` text NOT NULL,
-  `PhoneNumber` text NOT NULL,
-  `Password` text NOT NULL,
-  `Permission` varchar(16) NOT NULL
+  `ID` bigint(255) NOT NULL,
+  `Account` longtext NOT NULL,
+  `Email` longtext NOT NULL,
+  `Password` longtext NOT NULL,
+  `Permission` varchar(32) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `User`
 --
 
-INSERT INTO `User` (`Account`, `RealName`, `Email`, `Birthday`, `PhoneNumber`, `Password`, `Permission`) VALUES
-('admin', 'Administrator', 'Administrator@email.com', '0000-00-00', '0912345678', '$2y$10$BAjqe/VnBFt0L6P/JsOLFueDf/yFA0TFRtSR2tMpuSVCFhPHxB8Y2', 'ADMIN');
+INSERT INTO `User` (`ID`, `Account`, `Email`, `Password`, `Permission`) VALUES
+(0, 'admin', 'admin1234@hostmail.com', 'admin', 'ADMIN'),
+(10, 'user000', 'user000@hotmail.com', 'user000', 'USER');
 
 --
 -- Indexes for dumped tables
@@ -52,6 +51,7 @@ INSERT INTO `User` (`Account`, `RealName`, `Email`, `Birthday`, `PhoneNumber`, `
 -- Indexes for table `User`
 --
 ALTER TABLE `User`
+  ADD PRIMARY KEY (`ID`),
   ADD UNIQUE KEY `Permission` (`Permission`);
 COMMIT;
 
